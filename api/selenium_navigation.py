7b9fac685563d3800a64d6bac68979349821feba
Kangze Huang <kangzehuang@MacBook-Pro.lan>
2017-03-05 02:30:29 -0500

    added process

---
+++ api/selenium_navigation.py
@@ -0,0 +1,33 @@
+from api.propBasics_Selenium import getPropBasics_Selenium
+from api.propBasics_NLP import getPropBasics_NLP
+from api.demographics import getDemographics
+from api.images import extractImages
+from api.propBasics_GPS import getPropBasics_GPS
+
+def beginScript():
+
+# takes 2 args, a driver instance and a query string
+def buildProperty(driver, url, city, property_type):
+    driver.get(url)
+    try:
+        statsTable = driver.find_elements_by_xpath("//table[@class='ad-attributes']/tbody/tr")
+        paragraph = driver.find_element_by_xpath("//*[@id='UserContent']/table/tbody/tr/td/span")
+
+        leaseObj = {}
+        leaseObj = getPropBasics_Selenium(statsTable, leaseObj)
+        leaseObj = getPropBasics_NLP(paragraph, leaseObj)
+        leaseObj = demographics(paragraph, leaseObj)
+        leaseObj = extractImages(paragraph, leaseObj)
+        leaseObj = getPropBasics_GPS(paragraph, leaseObj)
+
+        leaseObj['city'] = city
+        leaseObj['company'] = "Private Landlord"
+        leaseObj['type'] = 'property_type'
+        leaseObj['active'] = True
+        leaseObj['claimed'] = False
+        leaseObj['deleted'] = False
+        leaseObj['thumbnail'] = 'https://s3.amazonaws.com/rentburrow-images/ideas%2540bytenectar.io/default_home_icon.png'
+        leaseObj['note'] = paragraph
+        print(leaseObj)
+    except TimeoutException:
+        print('TimeoutException')
--- main.py
+++ main.py
@@ -4,11 +4,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException, ElementNotVisibleException
 
 from api.selenium_navigation import beginScript
-from api.propBasics_Selenium import getPropBasics_Selenium
-from api.propBasics_NLP import getPropBasics_NLP
-from api.demographics import getDemographics
-from api.images import extractImages
-from api.propBasics_GPS import getPropBasics_GPS
+
 
 # creates the driver instance
 def init_driver():
@@ -17,33 +13,12 @@ def init_driver():
     driver.wait = WebDriverWait(driver, 5)
     return driver
 
-# takes 2 args, a driver instance and a query string
-def buildProperty(driver, url, city, property_type):
-    driver.get(url)
-    try:
-        tbody = driver.find_elements_by_xpath("//table[@class='ad-attributes']/tbody/tr")
-        paragraph = driver.find_element_by_xpath("//*[@id='UserContent']/table/tbody/tr/td/span")
-        leaseObj = {}
-        leaseObj = getTableAttrs(tbody, leaseObj)
-        leaseObj = getUtilsAttrs(paragraph, leaseObj)
-        leaseObj['city'] = city
-        leaseObj['company'] = "Private Landlord"
-        leaseObj['type'] = 'property_type'
-        leaseObj['active'] = True
-        leaseObj['claimed'] = False
-        leaseObj['deleted'] = False
-        leaseObj['thumbnail'] = 'https://s3.amazonaws.com/rentburrow-images/ideas%2540bytenectar.io/default_home_icon.png'
-        leaseObj['note'] = paragraph
-        print(leaseObj)
-    except TimeoutException:
-        print('TimeoutException')
-
 
 # __name__ is the python running script. if this file is run directly, then __name__ == "__main__"
 # if we imported this script, then __name__ != __main__
 if __name__ == "__main__":
     driver = init_driver()
     url = "http://www.kijiji.ca/v-house-rental/kitchener-waterloo/waterloo-townhome-for-rent-laurelwood/1241107253"
-    getPropertyDetails(driver, url, 'Toronto', 'house')
+    beginScript(driver, url, 'Toronto', 'house')
     time.sleep(5)
     driver.quit()
