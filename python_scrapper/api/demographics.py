c4935dc299a4ffb83d6a5c0f4b287d1fe875b75a
Kangze Huang <kangzehuang@MacBook-Pro.local>
2017-03-05 13:50:49 -0500

    updated

--- python_scrapper/api/demographics.py
+++ python_scrapper/api/demographics.py
@@ -13,10 +13,10 @@ def getDemographics(paragraph, leaseObj):
 
 
 def seniors(paragraph, leaseObj):
-    leaseObj['demographics'].append()
     try:
-        old = re.search(r'(quiet)|(No Loud Noise)|(inclusive)|(senior)|(family friendly)', paragraph, re.I).group()
+        old = re.search(r'(quiet)|(no loud)|(noise)|(senior)|(family friendly)', paragraph, re.I).group()
         print(old)
+        leaseObj['demographics'].append(old)
     except:
         print('NOT FOUND ==> # seniors')
         pass
@@ -31,8 +31,9 @@ def seniors(paragraph, leaseObj):
 
 def collegeStudents(paragraph, leaseObj):
     try:
-        students = re.search(r'(parks)|(nearby subway)|(downtown core)', paragraph, re.I).group()
+        students = re.search(r'(parks)|(near subway)|(downtown(|(core)|(campus)', paragraph, re.I).group()
         print(students)
+        leaseObj['demographics'].append(students)
     except:
         print('NOT FOUND ==> # students')
         pass
@@ -45,8 +46,9 @@ def collegeStudents(paragraph, leaseObj):
 
 def youngProfessionals(paragraph, leaseObj):
     try:
-        young_workers = re.search(r'(near subway)|(thriving atmosphere)|(near entertainment district)', paragraph, re.I).group()
+        young_workers = re.search(r'(near subway)|(thriving(|(atmosphere)|(near entertainment district)', paragraph, re.I).group()
         print(young_workers)
+        leaseObj['demographics'].append(young_workers)
     except:
         print('NOT FOUND ==> # young professionals')
         pass
@@ -69,6 +71,7 @@ def impoverishedImmigrants(paragraph, leaseObj):
     try:
         low_income = re.search(r'(low cost)|(diverse community)', paragraph, re.I).group()
         print(low_income)
+        leaseObj['demographics'].append(low_income)
     except:
         print('NOT FOUND ==> # low income')
         pass
@@ -81,6 +84,7 @@ def investorImmigrants(paragraph, leaseObj):
     try:
         high_income = re.search(r'(high end)|(gated community)|(uptown)', paragraph, re.I).group()
         print(high_income)
+        leaseObj['demographics'].append(high_income)
     except:
         print('NOT FOUND ==> high income')
         pass
@@ -91,8 +95,9 @@ def investorImmigrants(paragraph, leaseObj):
 
 def youngCouples(paragraph, leaseObj):
     try:
-        couples = re.search(r'(young professionals)|(excellent place to build relationship)', paragraph, re.I).group()
+        couples = re.search(r'(young professionals)|(build relationships)', paragraph, re.I).group()
         print(couples)
+        leaseObj['demographics'].append(couples)
     except:
         print('NOT FOUND ==> # couples')
         pass
@@ -102,8 +107,9 @@ def youngCouples(paragraph, leaseObj):
 
 def family(paragraph, leaseObj):
     try:
-        families = re.search(r'(family friendly community)|(parks)|(located near public school)', paragraph, re.I).group()
+        families = re.search(r'(family friendly)|(parks)|(public school)', paragraph, re.I).group()
         print(families)
+        leaseObj['demographics'].append(families)
     except:
         print('NOT FOUND ==> # family')
         pass
@@ -114,8 +120,9 @@ def family(paragraph, leaseObj):
 
 def hipYoungsters(paragraph, leaseObj):
     try:
-        hipsters = re.search(r'(vibrant area)|(near entertainment district)|(freash new design)', paragraph, re.I).group()
+        hipsters = re.search(r'(cultural)|(alive)|(entertainment)|(fresh)|(vibrant)|(thriving)', paragraph, re.I).group()
         print(hipsters)
+        leaseObj['demographics'].append(hipsters)
     except:
         print('NOT FOUND ==> # hipsters')
         pass
@@ -126,8 +133,9 @@ def hipYoungsters(paragraph, leaseObj):
 
 def generalDemands(paragraph, leaseObj):
     try:
-        gen_public = re.search(r'(close to grocery store)|(walking distance to bus)|(near mall)', paragraph, re.I).group()
+        gen_public = re.search(r'(grocery)|(bus)|(mall)', paragraph, re.I).group()
         print(gen_public)
+        leaseObj['demographics'].append(gen_public)
     except:
         print('NOT FOUND ==> # General Public')
         pass
--- python_scrapper/api/propBasics_NLP.py
+++ python_scrapper/api/propBasics_NLP.py
@@ -1,19 +1,19 @@
 import re
 
 def getPropBasics_NLP(paragraph, leaseObj):
-    leaseObj = getUtils(paragraph, leaseObj)
-    leaseObj = getWater(paragraph, leaseObj)
-    leaseObj = getHeat(paragraph, leaseObj)
-    leaseObj = getElectric(paragraph, leaseObj)
-    leaseObj = getFurnished(paragraph, leaseObj)
-    leaseObj = getParking(paragraph, leaseObj)
-    leaseObj = getFreeParking(paragraph, leaseObj)
-    leaseObj = getInternet(paragraph, leaseObj)
-    leaseObj = getAc(paragraph, leaseObj)
-    leaseObj = getGym(paragraph, leaseObj)
-    leaseObj = getLaundry(paragraph, leaseObj)
-    leaseObj = getFurnished(paragraph, leaseObj)
-    leaseObj = getRoomInfo(paragraph, leaseObj)
+    # leaseObj = getUtils(paragraph, leaseObj)
+    # leaseObj = getWater(paragraph, leaseObj)
+    # leaseObj = getHeat(paragraph, leaseObj)
+    # leaseObj = getElectric(paragraph, leaseObj)
+    # leaseObj = getFurnished(paragraph, leaseObj)
+    # leaseObj = getParking(paragraph, leaseObj)
+    # leaseObj = getFreeParking(paragraph, leaseObj)
+    # leaseObj = getInternet(paragraph, leaseObj)
+    # leaseObj = getAc(paragraph, leaseObj)
+    # leaseObj = getGym(paragraph, leaseObj)
+    # leaseObj = getLaundry(paragraph, leaseObj)
+    # leaseObj = getFurnished(paragraph, leaseObj)
+    # leaseObj = getRoomInfo(paragraph, leaseObj)
     return leaseObj
 
 def setAllUtils(leaseObj):
@@ -26,179 +26,246 @@ def setAllUtils(leaseObj):
 
 # catch-all utilities incl
 # in order of broad-search first
-def getUtils(paragraph, leaseObj):
+# def getUtils(paragraph, leaseObj):
+#
+#     # amenities
+#     try:
+#         hydro_and_internet_incl = re.search(r'(amenities incl)|(amenities included)|(all amenities)', paragraph, re.I).group()
+#         print(hydro_and_internet_incl)
+#         leaseObj = setAllUtils(leaseObj)
+#     except:
+#         print('NOT FOUND ==> # amenities')
+#         pass
+#
+#     # all utilities
+#     try:
+#         all_utilities = re.search(r'(all utilities)|(all utils)|(utilities included)|(utilities incl)|(utils incl)|(utils included)', paragraph, re.I).group()
+#         print(all_utilities)
+#         leaseObj = setAllUtils(leaseObj)
+#     except:
+#         print('NOT FOUND ==> # all utilities')
+#         pass
+#
+#     # all inclusive
+#     try:
+#         all_inclusive = re.search(r'(all inclusive)|(all included)|(everything included)', paragraph, re.I).group()
+#         print(hydro_and_internet_incl)
+#         leaseObj = setAllUtils(leaseObj)
+#         leaseOjb['utils_list']['furniture'] = True
+#     except:
+#         print('NOT FOUND ==> # all inclusive')
+#         pass
+#
+#     # plus utilities
+#     try:
+#         hydro_and_internet_incl = re.search(r'(hydro and internet included)|(hydro and internet incl)|(includes hydro and internet)', paragraph, re.I).group()
+#         print(hydro_and_internet_incl)
+#         leaseObj = setAllUtils(leaseObj)
+#     except:
+#         print('NOT FOUND ==> # plus utilities')
+#         pass
+#
+#     # utilities included
+#     try:
+#         hydro_and_internet_incl = re.search(r'(hydro and internet included)|(hydro and internet incl)|(includes hydro and internet)', paragraph, re.I).group()
+#         print(hydro_and_internet_incl)
+#         leaseObj = setAllUtils(leaseObj)
+#     except:
+#         print('NOT FOUND ==> # utilities included')
+#         pass
+#
+#     # water, electric, internet included
+#     try:
+#         hydro_and_internet_incl = re.search(r'(hydro and internet included)|(hydro and internet incl)|(includes hydro and internet)', paragraph, re.I).group()
+#         print(hydro_and_internet_incl)
+#         leaseObj = setAllUtils(leaseObj)
+#     except:
+#         print('NOT FOUND ==> # water, electric, internet included')
+#         pass
+#
+#     print("------------ next regex ------------")
+#     return leaseObj
+#
+# def getWater(paragraph, leaseObj):
+#    if leaseObj['utils_list']['water'] == True:
+#         # water extra, not included
+#         try:
+#             water_extra = re.search(r'(water extra, not included)|(water not included)', paragraph, re.I).group() # water extra, not included
+#             print(water_extra)
+#         except:
+#             print("NOT FOUND ==> # water extra, not include")
+#             pass
+#     else:
+#         try:
+#             water_incl = re.search(r'(water included)', paragraph, re.I).group() # water incl
+#             print(water_incl)
+#         except:
+#             print ("NOT FOUND ==> water included")
+#     return leaseObj
+#
+# def getHeat(paragraph, leaseObj):
+#     if leaseObj['utils_list']['heat'] == True:
+#         # heat extra, heating not incl
+#         try:
+#             heat_extra = re.search(r'(heat extra, not included)|(heat not included)', paragraph, re.I).group()
+#             print(heat_extra)
+#         except:
+#             print("NOT FOUND ==> # heat extra, not included")
+#         # gas extra, gas not incl
+#         try:
+#             gas_extra = re.search(r'(gas extra, not included)|(gas not included)', paragraph, re.I).group()
+#     else:
+#         # gas incl
+#         try:
+#             gas_incl = re.search(r'(gas included)', paragraph, re.I).group()
+#         except:
+#             print("NOT FOUND ==> # gas included")
+#         # heating incl
+#         try:
+#             heating_incl = re.search(r'(heating included)', paragraph, re.I).group()
+#         except:
+#             print("NOT FOUND ==> # heat included")
+#     return leaseObj
+#
+# def getElectric(paragraph, leaseObj):
+#     if leaseObj['utils_list']['electric'] == True:
+#         # hydro extra, not included
+#         try:
+#             hydro_extra = re.search(r'(hydro is extra, not included)| (extra hydro, not included)', paragraph, re.I).group()
+#         except:
+#             print("NOT FOUND ==> # extra hydro")
+#         # electricity, electric not included
+#         try:
+#             electric_extra = re.search(r'(electric is extra, not included)|(extra electrical, not included)', paragraph, re.I).group()
+#         except:
+#             print("NOT FOUND ==> # extra electrical")
+#     else:
+#         # electric incl
+#         try:
+#             elec_incl = re.search(r'(electrical included)|(included electrical)', paragraph, re.I).group()
+#         except:
+#             print("NOT FOUND ==> # electrical included")
+#         # power incl
+#         try:
+#             power_incl = re.search(r'(power included)|(included power)', paragraph, re.I).group()
+#         except:
+#             print("NOT FOUND ==> # power included")
+#     return leaseObj
+#
+# def getFurnished(paragraph, leaseObj):
+#     # fully furnished
+#     try:
+#         fully_furn = re.search(r'(fully furnished)', paragraph, re.I).group()
+#     except:
+#         print("NOT FOUND ==> # fully furnished")
+#     # furniture included
+#     try:
+#         furn_incl = re.search(r'(furniture included)|(included furnature)', paragraph, re.I).group()
+#     except:
+#         print("NOT FOUND ==> # furniture included")
+#     return leaseObj
+#
+# def getParking(paragraph, leaseObj):
+#     # parking extra
+#     try:
+#         parking_extra = re.search(r'(extra parking)|(parking is extra)', paragraph, re.I).group()
+#     except:
+#         print("NOT FOUND ==> # extra parking")
+#     # parking available
+#     try:
+#         parking_avail = re.search(r'(available parking)|(parking is available)', paragraph, re.I).group()
+#     except:
+#         print("NOT FOUND ==> # available parking")
+#     return leaseObj
+#
+# def getFreeParking(paragraph, leaseObj):
+#     # free parking, parking free
+#     try:
+#         free_parking = re.search(r'(free parking)|(parking is free)', paragraph, re.I).group()
+#     except:
+#         print("NOT FOUND ==> # free parking")
+#     return leaseObj
+#
+# def getInternet(paragraph, leaseObj):
+#     # unlimited wifi
+#     try:
+#         unlim_wifi = re.search(r'(unlimited wifi)|(wifi is unlimited)', paragraph, re.I).group()
+#     except:
+#         print("NOT FOUND ==> # unlimited wifi")
+#     # high speed internet
+#     try:
+#         high_speed_int = re.search(r'(high speed internet included)|(includes high speed internet)', paragraph, re.I).group()
+#     except:
+#         print("NOT FOUND ==> # high speed internet")
+#     # unlimited internet
+#     try:
+#         unlim_int = re.search(r'(unlimited internet provided)', paragraph, re.I).group()
+#     except:
+#         print("NOT FOUND ==> # unlimited internet provided")
+#     # internet extra
+#     try:
+#         int_extra = re.search(r'(internet is extra)|(extra internet)', paragraph, re.I).group()
+#     except:
+#         print("NOT FOUND ==> # internet is extra")
+#     # free internet
+#     try:
+#         free_int = re.search(r'(Free Internet)|(internet is free)', paragraph, re.I).group()
+#     except:
+#         print("NOT FOUND ==> # free internet")
+#     return leaseObj
+#
+#
+# def getAc(paragraph, leaseObj):
+#     # air-conditioned, air conditioning
+#     try:
+#         air_cond = re.search(r'(air conditioning is provided)|(provided with air conditioning)', paragraph, re.I).group()
+#     except:
+#         print("NOT FOUND ==> # provided air conditioning")
+#     # A/C, AC
+#     try:
+#         AC = re.search(r'(air conditioning is NOT provided)|(AC Not provided)', paragraph, re.I).group()
+#     except:
+#         print("NOT FOUND ==> # air conditioning")
+#     return leaseObj
+#
+# def getGym(paragraph, leaseObj):
+#     # gym
+#     try:
+#         gym = re.search(r'(gym is provided)|(access to gym)', paragraph, re.I).group()
+#     except:
+#         print("NOT FOUND ==> # gym provided")
+#     # fitness room
+#     try:
+#         fitness_room = re.search(r'(fitness room is provided)|(access to a fitness room)', paragraph, re.I).group()
+#     except:
+#         print("NOT FOUND ==> # fitness room")
+#     return leaseObj
+#
+# def getLaundry(paragraph, leaseObj):
+#     # laundry
+#     # coin laundry
+#     # laundry onsite
+#     try:
+#         laundry = re.search(r'(laundry machine provided)|(avialble laundry machine)', paragraph, re.I).group()
+#     except:
+#         print("NOT FOUND ==> laundry")
+#     return leaseObj
 
-    # amenities
-    try:
-        hydro_and_internet_incl = re.search(r'(amenities incl)|(amenities included)|(all amenities)', paragraph, re.I).group()
-        print(hydro_and_internet_incl)
-        leaseObj = setAllUtils(leaseObj)
-    except:
-        print('NOT FOUND ==> # amenities')
-        pass
+# def getFurnished(element, leaseObj):
+#     furnished = element[8].find_element_by_xpath("//*[@id='itemdetails']/div[2]/table/tbody/tr[9]/td").get_attribute('innerHTML')
+#     matched_furnished = re.search(r'No|Yes', furnished).group()
+#     print(matched_furnished)
+#     if matched_furnished == 'Yes':
+#         return True
+#     else:
+#         return False
 
-    # all utilities
-    try:
-        all_utilities = re.search(r'(all utilities)|(all utils)|(utilities included)|(utilities incl)|(utils incl)|(utils included)', paragraph, re.I).group()
-        print(all_utilities)
-        leaseObj = setAllUtils(leaseObj)
-    except:
-        print('NOT FOUND ==> # all utilities')
-        pass
-
-    # all inclusive
-    try:
-        all_inclusive = re.search(r'(all inclusive)|(all included)|(everything included)', paragraph, re.I).group()
-        print(hydro_and_internet_incl)
-        leaseObj = setAllUtils(leaseObj)
-        leaseOjb['utils_list']['furniture'] = True
-    except:
-        print('NOT FOUND ==> # all inclusive')
-        pass
-
-    # plus utilities
-    try:
-        hydro_and_internet_incl = re.search(r'(hydro and internet included)|(hydro and internet incl)|(includes hydro and internet)', paragraph, re.I).group()
-        print(hydro_and_internet_incl)
-        leaseObj = setAllUtils(leaseObj)
-    except:
-        print('NOT FOUND ==> # plus utilities')
-        pass
-
-    # utilities included
-    try:
-        hydro_and_internet_incl = re.search(r'(hydro and internet included)|(hydro and internet incl)|(includes hydro and internet)', paragraph, re.I).group()
-        print(hydro_and_internet_incl)
-        leaseObj = setAllUtils(leaseObj)
-    except:
-        print('NOT FOUND ==> # utilities included')
-        pass
-
-    # water, electric, internet included
-    try:
-        hydro_and_internet_incl = re.search(r'(hydro and internet included)|(hydro and internet incl)|(includes hydro and internet)', paragraph, re.I).group()
-        print(hydro_and_internet_incl)
-        leaseObj = setAllUtils(leaseObj)
-    except:
-        print('NOT FOUND ==> # water, electric, internet included')
-        pass
-
-    print("------------ next regex ------------")
-    return leaseObj
-
-def getWater(paragraph, leaseObj):
-   if leaseObj['utils_list']['water'] == True:
-        # water extra, not included
-        try:
-            water_extra = re.search(r'(water extra, not included)|(water not included)', paragraph, re.I).group() # water extra, not included
-            print(water_extra)
-        except:
-            print("NOT FOUND ==> # water extra, not include")
-            pass
-        return leaseObj
-    else:
-        try:
-            water_incl = re.search(r'(water included)', paragraph, re.I).group() # water incl
-            print(water_incl)
-        except:
-            print ("NOT FOUND ==> water included")
-        return leaseObj
-
-def getHeat(paragraph, leaseObj):
-    if leaseObj['utils_list']['heat'] == True:
-        # heat extra, heating not incl
-        try:
-            heat_extra = re.search(r'(heat extra, not included)|(heat not included)', paragraph, re.I).group()
-            print(heat_extra)
-        except:
-            print("NOT FOUND ==> # heat extra, not included")
-        # gas extra, gas not incl
-        try:
-            gas_extra = re.search(r'(gas extra, not included)|(gas not included)', paragraph, re.I).group()
-        return leaseObj
-    else:
-        # gas incl
-        try:
-            gas_incl = re.search(r'(gas included)', paragraph, re.I).group()
-        except:
-            print("NOT FOUND ==> # gas included")
-        # heating incl
-        try:
-            heating_incl = re.search(r'(heating included)', paragraph, re.I).group()
-        except:
-            print("NOT FOUND ==> # heat included")
-        return leaseObj
-
-def getElectric(paragraph, leaseObj):
-    if leaseObj['utils_list']['electric'] == True:
-        # hydro extra, not included
-        try:
-            hydro_extra = re.search(r'(hydro is extra, not included)| (extra hydro, not included)', paragraph, re.I).group()
-        except:
-            print("NOT FOUND ==> # extra hydro")
-        # electricity, electric not included
-        try:
-            electric_extra = re.search(r'(electric is extra, not included)|(extra electrical, not included)', paragraph, re.I).group()
-        return leaseObj
-    else:
-        elec_incl = re.search(r'', paragraph, re.I).group() # electric incl
-        power_incl = re.search(r'', paragraph, re.I).group()    # power incl
-        return leaseObj
-
-def getFurnished(paragraph, leaseObj):
-    fully_furn = re.search(r'', paragraph, re.I).group()   # fully furnished
-    furn_incl = re.search(r'', paragraph, re.I).group()   # furniture included
-    return leaseObj
-
-def getParking(paragraph, leaseObj):
-    parking_extra = re.search(r'', paragraph, re.I).group()   # parking extra
-    parking_avail = re.search(r'', paragraph, re.I).group() # parking available
-    return leaseObj
-
-def getFreeParking(paragraph, leaseObj):
-    free_parking = re.search(r'', paragraph, re.I).group()   # free parking, parking free
-    return leaseObj
-
-def getInternet(paragraph, leaseObj):
-    unlim_wifi = re.search(r'', paragraph, re.I).group()   # unlimited wifi
-    high_speed_int = re.search(r'', paragraph, re.I).group()   # high speed internet
-    unlim_int = re.search(r'', paragraph, re.I).group()   # unlimited internet
-    int_extra = re.search(r'', paragraph, re.I).group()   # internet extra
-    free_int = re.search(r'', paragraph, re.I).group()   # free internet
-    return leaseObj
-
-
-def getAc(paragraph, leaseObj):
-    air_cond = re.search(r'', paragraph, re.I).group()   # air-conditioned, air conditioning
-    AC = re.search(r'', paragraph, re.I).group() # A/C, AC
-    return leaseObj
-
-def getGym(paragraph, leaseObj):
-    gym = re.search(r'', paragraph, re.I).group() # gym
-    fitness_room = re.search(r'', paragraph, re.I).group() # fitness room
-    return leaseObj
-
-def getLaundry(paragraph, leaseObj):
-    # laundry
-    # coin laundry
-    # laundry onsite
-    return leaseObj
-
-def getFurnished(element, leaseObj):
-    furnished = element[8].find_element_by_xpath("//*[@id='itemdetails']/div[2]/table/tbody/tr[9]/td").get_attribute('innerHTML')
-    matched_furnished = re.search(r'No|Yes', furnished).group()
-    print(matched_furnished)
-    if matched_furnished == 'Yes':
-        return True
-    else:
-        return False
-
-def getRoomInfo(paragraph, leaseObj):
-    # spacious rooms
-    # cozy rooms
-    # big rooms, large rooms
-    leaseObj['rooms']['room_desciptions'] = []
-    return leaseObj
+# def getRoomInfo(paragraph, leaseObj):
+#     # spacious rooms
+#     # cozy rooms
+#     # big rooms, large rooms
+#     leaseObj['rooms']['room_desciptions'] = []
+#     return leaseObj
 
 
 
